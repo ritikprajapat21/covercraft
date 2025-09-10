449d5eb1c4cf24cbac2d028369b6c75489fcb836
Ritik <ritikprajapati084@gmail.com>
2025-09-10 10:16:46 +0530

    Update: No need for now this

--- auth.ts
+++ auth.ts
@@ -1,36 +1,13 @@
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import { accounts, db, sessions, users, verificationTokens } from "./db";
 
-// fix: save user in db
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  adapter: DrizzleAdapter(db, {
-    usersTable: users,
-    accountsTable: accounts,
-    sessionsTable: sessions,
-    verificationTokensTable: verificationTokens,
-  }),
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  callbacks: {
-    jwt({ token, user }) {
-      if (user) {
-        token.isPro = user.isPro;
-      }
-      return token;
-    },
-    session({ session, token }) {
-      if (token) {
-        session.user.isPro = token.isPro as boolean;
-      }
-      return session;
-    },
-  },
   session: {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60,
